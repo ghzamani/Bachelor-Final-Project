15b5bb80a52e0dcc7553189b030ab29a3884a947
ShMob <mobashers313@gmail.com>
2022-09-05 13:27:28 +0430

    save metrics from categories to a file for image captioning

--- clip_prefix_caption/model/zero_shot.py
+++ clip_prefix_caption/model/zero_shot.py
@@ -28,19 +28,26 @@ def main():
     # prefix_dim = args.prefix_size
     if args.categories_path != "":
         categories = ['cars', 'ceremonies', 'food', 'indoor', 'ashkhas', 'sport']
+    
+        args.mapping_type = {'mlp': MappingType.MLP, 'transformer': MappingType.Transformer}[args.mapping_type]
+
+        predictor = Predictor(args.model_weights, mapping_type=args.mapping_type,clip_length=args.prefix_length_clip, num_layers=args.num_layers,
+        is_eng= (args.language == "english"),prefix_length=args.prefix_length, prefix_size=args.prefix_size, clip_model=args.clip_model_type)
+        prefix = f'zero_shot_{args.language}'
+        metrics_dict = {}
         for category in categories:
             print("###### Using category", category, "#######")
             args.test_data = f'{args.categories_path}{category}_test'
             if args.language == 'english':
                 args.test_data = args.test_data + "_eng"
             args.test_data = args.test_data + ".json"
-            args.mapping_type = {'mlp': MappingType.MLP, 'transformer': MappingType.Transformer}[args.mapping_type]
-
-            predictor = Predictor(args.model_weights, mapping_type=args.mapping_type,clip_length=args.prefix_length_clip, num_layers=args.num_layers,
-            is_eng= (args.language == "english"),prefix_length=args.prefix_length, prefix_size=args.prefix_size, clip_model=args.clip_model_type)
-            prefix = f'zero_shot_{args.language}'
             predictions, targets = predictor.test(args.test_data, args.image_path, use_beam_search=True, output_name=prefix)
-            print(evaluate_metrics(predictions, targets, is_eng=(args.language == "english")))
+            metrics = evaluate_metrics(predictions, targets, is_eng=(args.language == "english"))
+            print(metrics)
+            metrics_dict[category] = metrics
+        
+        with open(f'{prefix}_metrics.json', 'w', encoding='utf-8') as f:
+            json.dump(metrics_dict, f, ensure_ascii=False, indent=4)
     else:
         args.mapping_type = {'mlp': MappingType.MLP, 'transformer': MappingType.Transformer}[args.mapping_type]
 
