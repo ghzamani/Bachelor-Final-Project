b2d47a1cbc1afb27603b547beee88f9be1a323dd
ShMob <mobashers313@gmail.com>
2022-09-05 13:10:23 +0430

    Add code to test all categories for image captioning

--- clip_prefix_caption/model/zero_shot.py
+++ clip_prefix_caption/model/zero_shot.py
@@ -18,6 +18,7 @@ def main():
     parser.add_argument('--mapping_type', type=str, default='transformer', help='mlp/transformer')
     parser.add_argument('--num_layers', type=int, default=8)
     parser.add_argument('--model_weights', default='')
+    parser.add_argument('--categories_path', default='')
     parser.add_argument('--is_rn', dest='is_rn', action='store_true')
     parser.add_argument('--language', default="english", choices=('english', 'persian'))
     parser.add_argument('--clip_model_type', default="RN50x4", choices=('RN50', 'RN101', 'RN50x4', 'ViT-B/32'))
@@ -25,13 +26,29 @@ def main():
     # prefix_length = args.prefix_length
     # # prefix_dim = 640 if args.is_rn else 512
     # prefix_dim = args.prefix_size
-    args.mapping_type = {'mlp': MappingType.MLP, 'transformer': MappingType.Transformer}[args.mapping_type]
+    if args.categories_path != "":
+        categories = ['cars', 'ceremonies', 'food', 'indoor', 'ashkhas', 'sport']
+        for category in categories:
+            print("###### Using category", category, "#######")
+            args.test_data = f'{args.categories_path}{category}_test'
+            if args.language == 'english':
+                args.test_data = args.test_data + "_eng"
+            args.test_data = args.test_data + ".json"
+            args.mapping_type = {'mlp': MappingType.MLP, 'transformer': MappingType.Transformer}[args.mapping_type]
 
-    predictor = Predictor(args.model_weights, mapping_type=args.mapping_type,clip_length=args.prefix_length_clip, num_layers=args.num_layers,
-     is_eng= (args.language == "english"),prefix_length=args.prefix_length, prefix_size=args.prefix_size, clip_model=args.clip_model_type)
-    prefix = f'zero_shot_{args.language}'
-    predictions, targets = predictor.test(args.test_data, args.image_path, use_beam_search=True, output_name=prefix)
-    print(evaluate_metrics(predictions, targets, is_eng=(args.language == "english")))
+            predictor = Predictor(args.model_weights, mapping_type=args.mapping_type,clip_length=args.prefix_length_clip, num_layers=args.num_layers,
+            is_eng= (args.language == "english"),prefix_length=args.prefix_length, prefix_size=args.prefix_size, clip_model=args.clip_model_type)
+            prefix = f'zero_shot_{args.language}'
+            predictions, targets = predictor.test(args.test_data, args.image_path, use_beam_search=True, output_name=prefix)
+            print(evaluate_metrics(predictions, targets, is_eng=(args.language == "english")))
+    else:
+        args.mapping_type = {'mlp': MappingType.MLP, 'transformer': MappingType.Transformer}[args.mapping_type]
+
+        predictor = Predictor(args.model_weights, mapping_type=args.mapping_type,clip_length=args.prefix_length_clip, num_layers=args.num_layers,
+        is_eng= (args.language == "english"),prefix_length=args.prefix_length, prefix_size=args.prefix_size, clip_model=args.clip_model_type)
+        prefix = f'zero_shot_{args.language}'
+        predictions, targets = predictor.test(args.test_data, args.image_path, use_beam_search=True, output_name=prefix)
+        print(evaluate_metrics(predictions, targets, is_eng=(args.language == "english")))
 
 
 if __name__ == '__main__':
